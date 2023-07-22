cb98e5530594a5584153aa4ebe30303b02cbe61f
gabriel-henrique-oliveira-amaral-MB <gabrielmanoelbatista@gmail.com>
2023-07-21 21:23:51 -0300

    Finalizando parte 4

---
+++ frontend/src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'Javascript Assertivo', id: 2, src: livro},
+    {nome: 'Cangaceiro em Javascript', id: 3, src: livro},
+    {nome: 'Aventureiros do Java', id: 4, src: livro}
+]
+
